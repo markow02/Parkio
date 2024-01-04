e35a12a36bc54b799221dd928b7df2af28d240fd
markow02 <114979874+markow02@users.noreply.github.com>
2024-01-04 19:09:18 +0100

    Add files via upload

--- Klienci.cs
+++ Klienci.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +14,58 @@ namespace SystemParkingowy
 {
     public partial class Klienci : Form
     {
+
         public Klienci()
         {
             InitializeComponent();
         }
+
 
         private void btnNewClient_Click(object sender, EventArgs e)
         {
             DodajKlienta newClient = new DodajKlienta();
             newClient.Show();
         }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Klienci_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        public void loadData()
+        {
+            var database = new Database();
+                if (database.connect_db())
+                {
+                string query = "SELECT imie, nazwisko, telefon, pesel, ulica, nazwa_miejscowosci FROM klient " +
+                               "LEFT JOIN adres ON klient.id_adres = adres.id_adres " +
+                               "RIGHT JOIN miejscowosc ON adres.id_miejscowosc = miejscowosc.id_miejscowosc";
+
+                MySqlCommand mySqlCommand = new MySqlCommand(query);
+                mySqlCommand.Connection = database.mySqlConection;
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                adapter.SelectCommand = mySqlCommand;
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                BindingSource bindingSource = new BindingSource();
+                dataGridViewCustomers.AutoGenerateColumns = false;
+                bindingSource.DataSource = dt;
+                
+                dataGridViewCustomers.DataSource = bindingSource;
+                
+                database.close_db();
+
+
+                }
+                else{
+                    MessageBox.Show("Database error");
+                }
+        }
+
     }
 }
--- Wjazdy.Designer.cs
+++ Wjazdy.Designer.cs
@@ -29,17 +29,62 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            dataGridViewEntry = new DataGridView();
+            marka = new DataGridViewTextBoxColumn();
+            model = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewEntry).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Black", 36F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(480, 352);
+            label1.Location = new Point(393, 45);
             label1.Name = "label1";
             label1.Size = new Size(238, 65);
             label1.TabIndex = 3;
             label1.Text = "WJAZDY";
+            // 
+            // dataGridViewEntry
+            // 
+            dataGridViewEntry.AllowDrop = true;
+            dataGridViewEntry.AllowUserToAddRows = false;
+            dataGridViewEntry.BackgroundColor = SystemColors.Menu;
+            dataGridViewEntry.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewEntry.Columns.AddRange(new DataGridViewColumn[] { marka, model, Column3, Column4 });
+            dataGridViewEntry.GridColor = SystemColors.InactiveBorder;
+            dataGridViewEntry.Location = new Point(35, 147);
+            dataGridViewEntry.Name = "dataGridViewEntry";
+            dataGridViewEntry.RowTemplate.Height = 25;
+            dataGridViewEntry.Size = new Size(443, 85);
+            dataGridViewEntry.TabIndex = 4;
+            // 
+            // marka
+            // 
+            marka.DataPropertyName = "marka";
+            marka.HeaderText = "Marka";
+            marka.Name = "marka";
+            // 
+            // model
+            // 
+            model.DataPropertyName = "model";
+            model.HeaderText = "Model";
+            model.Name = "model";
+            // 
+            // Column3
+            // 
+            Column3.DataPropertyName = "nr_rejestracyjny";
+            Column3.HeaderText = "Num rej";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.DataPropertyName = "dt_wjazd";
+            Column4.HeaderText = "Data wjazdu";
+            Column4.Name = "Column4";
             // 
             // Wjazdy
             // 
@@ -47,10 +92,13 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Cyan;
             ClientSize = new Size(1138, 768);
+            Controls.Add(dataGridViewEntry);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Wjazdy";
             Text = "Logi";
+            Load += Wjazdy_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewEntry).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -58,5 +106,12 @@
         #endregion
 
         private Label label1;
+        private DataGridView dataGridViewEntry;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn marka;
+        private DataGridViewTextBoxColumn model;
     }
 }
